1e0c3f33cb92aa6a24246c31599561fec40840e2
Damiano Bacci <damiano.bacci@gmail.com>
2023-11-13 23:27:42 +0100

    Gigs page styling

--- pages/milan.js
+++ pages/milan.js
@@ -1,40 +1,43 @@
+import Image from "next/image";
+
 export default function Milan() {
   return (
     <>
       <h1 className="mt-20 mb-20 text-center text-3xl uppercase font-black">
         List of gigs in Milan
       </h1>
-      <table className="table-auto text-center m-auto mb-10">
+      <table className="table-fixed m-auto mb-10 text-xl border-separate">
         <thead className="uppercase">
           <tr className="p-4">
             <th className="p-4">Date</th>
             <th>Artist</th>
             <th>Venue</th>
             <th>Price</th>
-            <th>Link</th>
+            <th>Links</th>
           </tr>
         </thead>
         <tbody className="">
           <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>Malcolm Lockyer</td>
-            <td>1961</td>
-          </tr>
-          <tr>
-            <td>Witchy Woman</td>
-            <td>Witchy Woman</td>
-            <td>Witchy Woman</td>
-            <td>The Eagles</td>
-            <td>1972</td>
-          </tr>
-          <tr>
-            <td>Shining Star</td>
-            <td>Shining Star</td>
-            <td>Shining Star</td>
-            <td>Earth, Wind, and Fire</td>
-            <td>1975</td>
+            <td className="px-4 py-4">March, 22nd</td>
+            <td className="px-4 py-4">A Silver Mount Zion</td>
+            <td className="px-4 py-4">Magazzini Generali</td>
+            <td className="px-4 py-4">23€</td>
+            <td className="px-4 py-4">
+              <Image
+                className="inline mx-2"
+                src="/tickets.png"
+                alt="Tickets"
+                width={16}
+                height={16}
+              />
+              <Image
+                className="inline"
+                src="/instagram.png"
+                alt="Instagram"
+                width={16}
+                height={16}
+              />
+            </td>
           </tr>
         </tbody>
       </table>
