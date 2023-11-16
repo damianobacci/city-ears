130cbfd022f90a13ccf0fd2bde0454f15542be29
Damiano Bacci <damiano.bacci@gmail.com>
2023-11-16 21:44:07 +0100

    Row component

--- components/table/row.js
+++ components/table/row.js
@@ -1,25 +1,29 @@
+import Link from "next/link";
+
 export default function Row(props) {
   return (
     <tr>
-      <td className="px-4 py-4">March, 22nd</td>
-      <td className="px-4 py-4">A Silver Mount Zion</td>
-      <td className="px-4 py-4">Magazzini Generali</td>
-      <td className="px-4 py-4">23€</td>
+      <td className="px-4 py-4">{props.date}</td>
+      <td className="px-4 py-4">{props.artist}</td>
+      <td className="px-4 py-4">{props.venue}</td>
+      <td className="px-4 py-4">{props.price}€</td>
       <td className="px-4 py-4">
-        <Image
-          className="inline mx-2"
-          src="/tickets.png"
-          alt="Tickets"
-          width={16}
-          height={16}
-        />
-        <Image
+        <Link href={props.tickets}>
+          <Image
+            className="inline mx-2"
+            src="/tickets.png"
+            alt="Tickets"
+            width={16}
+            height={16}
+          />
+        </Link>
+        {/* <Image
           className="inline"
           src="/instagram.png"
           alt="Instagram"
           width={16}
           height={16}
-        />
+        /> */}
       </td>
     </tr>
   );
