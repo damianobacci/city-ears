617424a9a6a6dcb65c7c8b35e2b475940a4fed12
Damiano Bacci <damiano.bacci@gmail.com>
2023-11-16 22:12:56 +0100

    Vercel proto

--- pages/turin.js
+++ pages/turin.js
@@ -1,5 +1,46 @@
 import Header from "@/components/layout/header";
+import Table from "@/components/table/table";
+import Row from "@/components/table/row";
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    artist: "A Silver Mount Zion",
+    date: "22nd March",
+    venue: "Magazzini Generali",
+    price: 23,
+    tickets: "/milan",
+    instagram: "/milan",
+  },
+  {
+    id: 1,
+    artist: "A Silver Mount Zion",
+    date: "22nd March",
+    venue: "Magazzini Generali",
+    price: 23,
+    tickets: "/milan",
+    instagram: "/milan",
+  },
+  {
+    id: 1,
+    artist: "A Silver Mount Zion",
+    date: "22nd March",
+    venue: "Magazzini Generali",
+    price: 23,
+    tickets: "/milan",
+    instagram: "/milan",
+  },
+];
 
 export default function Turin() {
-  return <Header>List of gigs in Turin</Header>;
+  return (
+    <>
+      <Header>List of gigs in Turin</Header>
+      <Table>
+        {DUMMY_DATA.map((item) => (
+          <Row key={item.id} {...item} />
+        ))}
+      </Table>
+    </>
+  );
 }
