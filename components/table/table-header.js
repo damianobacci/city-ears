e644d17453df114c1c9658b74f2c545f7750c80c
Damiano Bacci <damiano.bacci@gmail.com>
2023-11-16 22:29:52 +0100

    Fix style

--- components/table/table-header.js
+++ components/table/table-header.js
@@ -2,10 +2,10 @@ export default function TableHeader(props) {
   return (
     <thead className="uppercase">
       <tr className="p-4">
-        <th className="p-4">Date</th>
-        <th>Artist</th>
+        <th className="p-4">📅</th>
+        <th>🎶</th>
         <th>📍</th>
-        <th>€</th>
+        <th>💶</th>
         <th>🔗</th>
       </tr>
     </thead>
