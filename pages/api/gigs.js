e8b6f44ae614865739c3e89505f878952c165d25
Damiano Bacci <damiano.bacci@gmail.com>
2023-11-27 10:16:11 +0100

    Connection to DB working

--- pages/api/gigs.js
+++ pages/api/gigs.js
@@ -6,23 +6,18 @@ export default function handler(req, res) {
   if (req.method === "GET") {
     res.status(200).json({ message: "Lolz" });
   } else if (req.method === "POST") {
-    console.log(req.body);
+    const newEvent = req.body;
+    console.log(newEvent);
     // if (!date || !city) {
     //   res.status(422).json({ message: "Invalid data" });
     //   return;
     //   //add other server-side validation!
     // }
-    // const newEvent = {
-    //   date,
-    //   city,
-    //   artist,
-    //   venue,
-    //   price,
-    //   tickets,
-    //   instagram,
-    //   facebook,
-    // };
-    MongoClient.connect(URI);
+
+    MongoClient.connect(URI).then((client) => {
+      const db = client.db();
+      db.collection("gigslist").insertOne(newEvent);
+    });
     res.status(201).json({ message: "Successfully stored event!" });
   }
 }
