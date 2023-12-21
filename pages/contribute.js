9030996eebb452f2b16e479ca5438b2d7c380f6f
Damiano Bacci <damiano.bacci@gmail.com>
2023-12-21 18:08:46 +0100

    Form in contribute page

--- pages/contribute.js
+++ pages/contribute.js
@@ -4,15 +4,47 @@ export default function Contribute() {
   return (
     <>
       <Header>Want to contribute?</Header>
-      <p className="m-auto text-center">
+      <p className="m-auto text-center my-2">
         Please fill out this form to request a login.
       </p>
-      <form action="" className="text-center border-4">
-        <label htmlFor="">Name</label>
-        <input type="text" />
-        <label htmlFor="">Email</label>
-        <input type="text" />
-        <button type="submit"></button>
+
+      <form className="max-w-sm mx-auto">
+        <div className="mb-5">
+          <div className="mb-5">
+            <label
+              for="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Name"
+              required
+            />
+          </div>
+          <label
+            for="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="name@email.com"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full uppercase"
+        >
+          Submit
+        </button>
       </form>
     </>
   );
